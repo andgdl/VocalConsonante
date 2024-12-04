f4b1d957b0adfae79677d0f5c3e9eb1122f120d7
Camila Medina <24030480@itcelaya.edu.mx>
2024-12-03 21:08:50 -0600

    Update VocalConsonante.java

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -10,11 +10,12 @@ public class VocalConsonante {
 }
 
 class Tipo() {
+    public static String determinaTipo(char ch){
     char ch = args[0].charAt(0);
 
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " es una vocal.");
         else 
             System.out.println(ch + " es una consonante."); 
-    
+    }
 }
