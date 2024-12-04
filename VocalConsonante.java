bdeb27e0b7a172740d60ac7b7bb288559a3d267c
andgdl <dcameme27@gmail.com>
2024-12-03 22:06:18 -0600

    Update VocalConsonante.java

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class VocalConsonante {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Tipo tp = new Tipo();
+       
+        while(true){
         System.out.println("Introduce una letra");
         char ch = sc.next().charAt(0);
         
-    } 
+        if(ch=='p'){
+            System.out.println("Programa finalizado");
+            break;
+        }
+        
+        tp.determinaTipo(ch);
+        }
+    }
 }
 
 class Tipo() {
     public static String determinaTipo(char ch){
-    char ch = args[0].charAt(0);
-
+        
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " es una vocal.");
         else 
